7531dc7f819603ff06f6c4b793bd570eef184329
Nhoj-Plummer <nhojplummer@gmail.com>
2025-02-16 13:20:11 -0500

    Adding sections to the project

--- src/components/SocialLink/SocialLink.jsx
+++ src/components/SocialLink/SocialLink.jsx
@@ -1,6 +1,17 @@
 import "@/components/SocialLink/SocialLink.css";
+import Image from "next/image";
+import ProfilePic from "/public/avatar-jessica.jpeg";
+
 const SocialLnk = () => {
-  return <div className="social-link-main ">SocialLink</div>;
+  return (
+    <div className="social-link-main">
+      <div className="social-link_image">
+        <Image src={ProfilePic} alt="face-image" />
+      </div>
+      <div className="social-link_text"></div>
+      <div className="social-link_links"></div>
+    </div>
+  );
 };
 
 export default SocialLnk;
