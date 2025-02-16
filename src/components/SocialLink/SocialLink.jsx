860f51eee985a5d595b6337e78489e4498891daa
Nhoj-Plummer <nhojplummer@gmail.com>
2025-02-16 13:24:57 -0500

    Adding more text

--- src/components/SocialLink/SocialLink.jsx
+++ src/components/SocialLink/SocialLink.jsx
@@ -5,10 +5,17 @@ import ProfilePic from "/public/avatar-jessica.jpeg";
 const SocialLnk = () => {
   return (
     <div className="social-link-main">
-      <div className="social-link_image">
-        <Image src={ProfilePic} alt="face-image" />
+      <div className="social-link_section1">
+        <div className="social-link_image">
+          <Image src={ProfilePic} alt="face-image" />
+        </div>
+        <div className="social-link_text">
+          <h1>Jessica Randall</h1>
+          <h2>London, United Kingdom</h2>
+          <p>"Front-end developer and avid reader."</p>
+        </div>
       </div>
-      <div className="social-link_text"></div>
+
       <div className="social-link_links"></div>
     </div>
   );
