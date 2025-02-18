b12c977b89c8c3bcfeb79f8fc2e44bac580e86c5
Nhoj-Plummer <nhojplummer@gmail.com>
2025-02-18 17:44:30 -0500

    CMopleted now Pull

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,13 @@ export default function Home() {
   return (
     <>
       <SocialLink />
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Nhoj Plummer</a>.
+      </div>
     </>
   );
 }
